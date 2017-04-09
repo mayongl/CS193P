0690547c80d7979cdadeb8694bd3679a42eb6485
Yonglin <mayongl@apache.org>
2017-04-09 12:26:45 +0800

    Add twitter framework for smashtag of Lecture 9

---
+++ Twitter/Twitter/Twitter.h
@@ -0,0 +1,19 @@
+//
+//  Twitter.h
+//  Twitter
+//
+//  Created by Yonglin Ma on 4/8/17.
+//  Copyright © 2017 Sixlivesleft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Twitter.
+FOUNDATION_EXPORT double TwitterVersionNumber;
+
+//! Project version string for Twitter.
+FOUNDATION_EXPORT const unsigned char TwitterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Twitter/PublicHeader.h>
+
+
